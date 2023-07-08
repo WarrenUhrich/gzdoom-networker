d25c1b0350dab7f310dec9e0189570201a016652
Warren <hello@warren.codes>
2023-07-08 15:50:15 -0600

    Add additional info; add player number; add custom WAD support

--- gzdoom-networker.bat
+++ gzdoom-networker.bat
@@ -8,6 +8,17 @@ echo:
 set GZDOOM_LOCATION=%LOCALAPPDATA%\Programs\gzdoom\gzdoom.exe
 echo Game Location: %GZDOOM_LOCATION%
 
+echo Would you like to use a custom WAD? (y/n) 
+set /p CUSTOM_WAD=
+
+IF %CUSTOM_WAD%=y (
+    echo:
+    echo:
+    echo Please paste the path for your custom WAD (right-click to paste):
+    set /p CUSTOM_WAD_PATH=
+    set GZDOOM_LOCATION=%GZDOOM_LOCATION% %CUSTOM_WAD_PATH%
+  )
+
 echo:
 echo:
 
@@ -42,7 +53,10 @@ echo The GZDoom Networker is now in Hosting Mode.
 echo:
 echo:
 
-set HOST_COMMAND=%GZDOOM_LOCATION% -host
+echo How many players are going to play? 
+set /p NUM_OF_PLAYERS=
+
+set HOST_COMMAND=%GZDOOM_LOCATION% -host %NUM_OF_PLAYERS%
 
 echo:
 echo:
@@ -50,10 +64,7 @@ echo:
 echo Which level would you like to use? 
 set /p GAME_LEVEL=
 
-set HOST_COMMAND=%HOST_COMMAND% -skill %GAME_LEVEL%
-
-echo:
-echo:
+set HOST_COMMAND=%HOST_COMMAND% -warp %GAME_LEVEL%
 
 echo Is this a deathmatch? (y/n) 
 set /p DEATHMATCH=
@@ -76,7 +87,10 @@ echo:
 FOR /f "tokens=1* delims=: " %%A IN (
   'nslookup myip.opendns.com. resolver1.opendns.com 2^>NUL^|find "Address:"'
 ) DO set ExtIP=%%B
-echo External IP is: %ExtIP%
+
+echo External IP used for join is: %ExtIP%
+echo:
+echo GZDoom uses port: 5029
 
 echo:
 echo:
